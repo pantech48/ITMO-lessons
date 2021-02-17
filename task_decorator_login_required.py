90642c15ae62adf1ce0fabf5d4c9d2f138d57992
Evgeniy Putrin <evgeniy.putrin@gmail.com>
2021-02-17 23:45:33 +0300

    finally

--- task_decorator_login_required.py
+++ task_decorator_login_required.py
@@ -1,27 +1,29 @@
 import hashlib
 
+
 def make_token(username, password):
     s = username + password
     return hashlib.md5(s.encode()).hexdigest()
+  
 
-with_err = 1
+
+fail = 1
 
 def login_required(func):
     def wrapper(*args, **kwargs):
-        with open('token.txt') as f:
-            auth = f.read().strip('\n ')
-        global with_err
-        misstake = 0
-        while with_err:
-            login = input()
-            password = input()
-            if make_token(login, password) == auth:
-                with_err = 0              
+        global fail
+        fail_count = 0    
+        while fail:
+            with open('token.txt') as f:
+                auth = f.read().strip('\n ')
+            username, password = input(), input()
+            user_data = make_token(username, password)
+            if user_data == auth:
+                fail = 0
+                break
             else:
-                misstake += 1
-                if misstake == 3:
-                    raise RuntimeError('Authentication failed')   
-        return func(*args, **kwargs)       
+                fail_count += 1
+            if fail_count >= 3:
+                raise RuntimeError('Authentication failed')
+        return func(*args, **kwargs)
     return wrapper
-
-
