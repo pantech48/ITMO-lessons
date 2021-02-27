3d5790b1e47942b63a22d2e6a0587fe731d1b51d
Evgeniy Putrin <evgeniy.putrin@gmail.com>
2021-02-27 04:32:08 +0300

    a

--- task_matrix_type.py
+++ task_matrix_type.py
@@ -1,41 +1,95 @@
-class Matrix:
-
-    def __init__(self, value):
-        self._value = value
-
-    def __add__(self, other):
-
-        def matrix_size(matrix):
-            line = len(matrix._value)
-            column = len(matrix._value[0])
-            return line, column
-
-        if matrix_size(self) == matrix_size(other):
-
-            add = list(zip(self, other))
-            for j, k in enumerate(add):
-                add[j] = add[j][j][j] + add[j][j+1][j]
-            
-            for j, k in enumerate(self):
-                for j, k in enumerate(other):
-
-
-    def __sub__(self, other):
-
-    def __mul__(self, other):
-
-    def get_transpose_matrix(self):
-        for m, n
-
-    def tolist(self):
-
-
-
 class MatrixSizeError(ArithmeticError):
-
-
-
+    """Matrix size error class."""
+    def __init__(self):
+        super().__init__('Incompatible matrix size')
 
 
 class OperandTypeError(TypeError):
+    """Operand type error class."""
+    def __init__(self, operand, operator):
+        super().__init__(
+            f"unsupported operator type(s) for {operator}: '{Matrix.__name__}' and '{type(operand).__name__}'"
+        )
+
+
+class Matrix:
+    """Class Matrix"""
+
+    def __init__(self, value):
+        """Inizializator of 'Matrix' object"""
+        self._value = value
+
+    def size(self):
+        """Size of matrix."""
+        size = {
+            'columns': len(self._value[0]),
+            'rows': len(self._value)
+        }
+        return size
+
+    def __add__(self, other):
+        """Addition operator."""
+
+        if not isinstance(other, Matrix):
+            raise OperandTypeError(other, '+')
+
+        if self.size() != other.size:
+            raise MatrixSizeError
+
+        result = [[self._value[i][j] + other._value[i][j] for j in range(self.size()['columns'])] for i in range(other.size()['rows'])]
+        return Matrix(result)
+
+
+    def __sub__(self, other):
+        """Substraction operator."""
+
+        if not isinstance(other, Matrix):
+            raise OperandTypeError(other, '-')
+
+        if self.size() != other.size:
+            raise MatrixSizeError
+
+        result = [[self._value[i][j] - other._value[i][j] for j in range(self.size()['columns'])] for i in range(other.size()['rows'])]
+        return Matrix(result)
+
+    def __mul__(self, other):
+        """Multiplication operator."""
+
+        if not (isinstance(other, Matrix) or isinstance(other, int) or isinstance(other, float)):
+            raise OperandTypeError(other, '*')
+
+        if isinstance(other, Matrix):
+
+            if len(self.get_transpose_matrix()._value) != len(other._value):
+                raise MatrixSizeError()
+
+            result = [[sum(a*b for a,b in zip(self_raw,other_col)) for other_col in zip(*other._value)] for self_raw in self._value]
+            return Matrix(result)
+        
+        result = [[elem*other for elem in row] for row in self._value]
+        return Matrix(result)
+
+    def get_transpose_matrix(self):
+        """Transpose operator."""
+        trans_matr = list(map(list, zip(*self._value)))
+        return Matrix(trans_matr)
+        
+    def tolist(self):
+        """Return matrix as 2D list."""
+        return self._value
+
+a = Matrix([
+    [1, -1],
+    [2, 0],
+    
+])
+
+b = Matrix([
+    [1, -1],
+    [2, 0],
+    
+])
+
+c = a * 3
+print(c.tolist() is c)
 
--- task_money_type.py
+++ task_money_type.py
@@ -1,39 +1,133 @@
 class OperandTypeError(TypeError):
+    """Operand type error class."""
+    def __init__(self, operand, operator=None):
+        if operator is not None:
+            super().__init__(
+                f"unsupported operator type(s) for {operator}: '{Money.__name__}' and '{type(operand).__name__}'"
+            )
+        else:
+            super().__init__(
+                f"comparison not supported between instances of '{Money.__name__}' and '{type(operand).__name__}'"
+            )
+        
 
 class MoneyArithmeticError(ArithmeticError):
+    """Money arithmetic error class."""
+    def __init__(self):
+        super().__init__('The coefficient should be positive')
+
 
 class Money:
+    """Money class."""
 
-    def __init__(self, whole, fracton=0):
-        self._whole = whole
-        self._fraction = fracton
-
-    def __add__(self, other):
-        if type(self) != type(other):
-            raise OperandTypeError('')
-        return Money(self._whole + other._whole, self._fraction + other._fracton)
-
-    def __sub__(self,other):
-        if type(self) != type(other):
-            raise OperandTypeError('')
-        return Money(self._whole - other._whole, self._fraction - other._fracton)
-
-    def __mul__(self, number):
-        if not (type(number) == type(int) or type(number) == type(float)):
-            raise OperandTypeError('')
-        mult = self._whole + self._fraction * 10**(-)
-        return Money(self._whole - other._whole, self._fraction - other._fracton)
-
-    def __truediv__(self, other):
-
-    def __eq__(self, other):
-
-    def __gt__(self, other):
-
-    def __ge__(self, other):
-
-    def __repr__(self):
+    def __init__(self, whole, fraction=0):
+        """Initialize object 'Money'."""
+        self._whole = whole 
+        self._fraction = fraction
+        self._int_curency = self._whole * 100 + self._fraction
 
     def get_whole(self):
+        """Getter for whole attr."""
+        return self._int_curency // 100
 
     def get_fraction(self):
+        """Getter for frection attr."""
+        return self._int_curency - self.get_whole()*100
+
+    def __repr__(self):
+        """Representation of object 'Money'."""
+        return '{}({}, {})'.format(
+                                self.__class__.__name__,
+                                self.get_whole(),     
+                                self.get_fraction()
+                                )
+
+    def __add__(self, other):
+        """Addition operator."""
+
+        if not isinstance(other, Money):
+            raise OperandTypeError(other, '+')
+
+        add = self._int_curency + other._int_curency
+        result = Money(0, add)
+        return Money(result.get_whole(), result.get_fraction())
+
+    def __sub__(self,other):
+        """Division operator."""
+
+        if not isinstance(other, Money):
+            raise OperandTypeError(other, '-')
+
+        sub = self._int_curency + other._int_curency
+        result = Money(0, sub)
+        return Money(result.get_whole(), result.get_fraction())
+
+    def __mul__(self, number):
+        """Division operator."""
+        
+        if not (isinstance(number, int) or isinstance(number, float)):
+            raise OperandTypeError(number, '*')
+
+        mult = self._int_curency * number
+        result = Money(0, mult)
+        return Money(result.get_whole(), result.get_fraction())
+        
+
+    def __truediv__(self, other):
+        """Division operator."""
+
+        if not (isinstance(other, int) or isinstance(other, Money)):
+            raise OperandTypeError(other, '/')
+
+        if isinstance(other, Money):
+            result = self._int_curency / other._int_curency
+            return float(result)
+        
+        if other <= 0:
+            raise MoneyArithmeticError()
+
+        div = round(self._int_curency / other)
+        result = Money(0, div)
+        return Money(result.get_whole(), result.get_fraction())
+
+
+    def __eq__(self, other):
+        """Division operator."""
+
+        if not isinstance(other, Money):
+            raise OperandTypeError(other)
+
+        result = self._int_curency == other._int_curency
+        return result
+
+
+
+    def __gt__(self, other):
+        """Division operator."""
+
+        if not isinstance(other, Money):
+            raise OperandTypeError(other)
+
+        result = self._int_curency > other._int_curency
+        return result
+
+    def __ge__(self, other):
+        """Division operator."""
+
+        if not isinstance(other, Money):
+            raise OperandTypeError(other)
+
+        result = self._int_curency >= other._int_curency
+        return result
+
+
+
+
+money1 = Money(593, 72)
+
+money2 = Money(0, 455)
+
+a = money1 / 74
+
+print(money1 != money2)
+print(a)
