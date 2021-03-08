f298816ef202080e711814b2d1f2c4aa9513c2ec
Evgeniy Putrin <evgeniy.putrin@gmail.com>
2021-03-08 18:30:33 +0300

    dfsdf

---
+++ task_validator.py
@@ -0,0 +1,66 @@
+from abc import ABCMeta, abstractmethod
+from datetime import datetime
+
+
+class Validator(metaclass=ABCMeta):
+
+    @abstractmethod
+    def validate(self, value):
+        """Метод принимает валидируемое значение"""
+
+
+class EMailValidator(Validator):
+    def __init__(self):
+        pass
+
+    def validate(self, value):
+        if not 0 < value.count('@') < 2:
+            return False
+        if value.startswith('@') or value.endswith('@'):
+            return False       
+        return True
+
+        
+class DateTimeValidator(Validator):
+    
+    def __init__(self, date_formats=[
+                                        '%Y-%m-%d',
+                                        '%Y-%m-%d %H:%M',
+                                        '%Y-%m-%d %H:%M:%S',
+                                        '%d.%m.%Y',
+                                        '%d.%m.%Y %H:%M',
+                                        '%d.%m.%Y %H:%M:%S',
+                                        '%d/%m/%Y',
+                                        '%d/%m/%Y %H:%M',
+                                        '%d/%m/%Y %H:%M:%S',
+                                    ]
+    ):
+        self.date_formats = date_formats
+
+    def validate(self, value): 
+        date_signs = [i for i in value if not i.isnumeric()]
+        fit_frmt = ''
+            
+        for frmt in self.date_formats:
+            frmt_signs = (i for i in frmt if not (i.isalpha() or i == '%'))      
+            if tuple(date_signs) == tuple(frmt_signs):
+                fit_frmt = frmt   
+                   
+        try:
+                datetime.strptime(value, fit_frmt)
+                return True
+        except ValueError:
+            return False
+
+
+class ChainValidator(Validator):
+    
+    def __init__(self, validators):
+        self.validators = validators
+
+    def validate(self, value):
+        for validator in self.validators:
+            if validator.validate(value):
+                continue
+            return False
+        return True
